4029d11f1886f3424cd405ae7791df621d3a2cfe
corneliouzbett <kbett68@gmail.com>
2022-01-31 12:34:07 +0300

    Add encounter_status queue_entry mapping table

---
+++ api/src/main/java/org/openmrs/module/queue/model/EncounterStatusQueue.java
@@ -0,0 +1,84 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.queue.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import java.util.Date;
+import java.util.UUID;
+
+import lombok.Data;
+import org.hibernate.search.annotations.Field;
+import org.openmrs.Auditable;
+import org.openmrs.Retireable;
+import org.openmrs.User;
+
+@Data
+@Entity
+@Table(name = "encounter_status_queue_map")
+public class EncounterStatusQueue implements Auditable, Retireable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "encounter_status_queue_id")
+	private Integer id;
+
+	@ManyToOne
+	@JoinColumn(name = "encounter_status_id", nullable = false)
+	private EncounterStatus encounterStatus;
+
+	//TODO Add relationship
+	@Column(name = "queue_entry_id", nullable = false)
+	private QueueEntry queueEntry;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "creator", updatable = false)
+	protected User creator;
+
+	@Column(name = "date_created", nullable = false, updatable = false)
+	private Date dateCreated;
+
+	@ManyToOne
+	@JoinColumn(name = "changed_by")
+	private User changedBy;
+
+	@Column(name = "date_changed")
+	private Date dateChanged;
+
+	@Column(name = "retired", nullable = false)
+	@Field
+	private Boolean retired = Boolean.FALSE;
+
+	@Column(name = "date_retired")
+	private Date dateRetired;
+
+	@ManyToOne
+	@JoinColumn(name = "retired_by")
+	private User retiredBy;
+
+	@Column(name = "retire_reason")
+	private String retireReason;
+
+	@Column(name = "uuid", unique = true, nullable = false, length = 38)
+	private String uuid = UUID.randomUUID().toString();
+
+	@Override
+	@Deprecated
+	public Boolean isRetired() {
+		return retired;
+	}
+}
