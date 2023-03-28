5649b26ce2d569253d11b98b79ff8acafbf56593
ojwanganto <ojwangwachiaje@gmail.com>
2023-03-28 18:33:10 +0300

    add endpoint for updating ticket assignment

--- api/src/main/java/org/openmrs/module/queue/api/digitalSignage/QueueTicketAssignments.java
+++ api/src/main/java/org/openmrs/module/queue/api/digitalSignage/QueueTicketAssignments.java
@@ -1,63 +1,103 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.module.queue.api.digitalSignage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Iterator;
 
 import org.apache.commons.lang3.StringUtils;
 import org.codehaus.jackson.node.JsonNodeFactory;
 import org.codehaus.jackson.node.ObjectNode;
 
-import java.util.Iterator;
+import javax.swing.*;
 
 /**
  * A utility class for updating details of active queue tickets
  */
 public class QueueTicketAssignments {
-
-    /**
-     * The object has:
-     * service point/room name as key for ease of search and update
-     * and object with status and ticket number
-     */
-    private static ObjectNode ACTIVE_QUEUE_TICKETS = JsonNodeFactory.instance.objectNode();
-
-    /**
-     * We want to control access to the ACTIVE_QUEUE_TICKETS so that requests are queued
-     * @param servicePointName
-     * @param ticketNumber
-     * @param status
-     */
-    synchronized public static void updateTicketAssignment(String servicePointName, String ticketNumber, String status) {
-        if (StringUtils.isNotBlank(servicePointName) && StringUtils.isNotBlank(ticketNumber) && StringUtils.isNotBlank(status)) {
-            if (ACTIVE_QUEUE_TICKETS.has(servicePointName)) { // check if there is an existing ticket assignment
-                //update the object with new information
-                ObjectNode tNode = (ObjectNode) ACTIVE_QUEUE_TICKETS.get(servicePointName);
-                tNode.put("status", status);
-                tNode.put("ticketNumber", ticketNumber);
-            } else {
-                // remove the ticket number from any assignment
-
-                Iterator<String> keys = ACTIVE_QUEUE_TICKETS.getFieldNames();
-
-                while (keys.hasNext()) { // check if the ticket number has active assignment to any of the service rooms
-                    String key = keys.next();
-                    ObjectNode obj = (ObjectNode) ACTIVE_QUEUE_TICKETS.get(key);
-                    if (obj.has(ticketNumber)) {
-                        // remove the object
-                        ACTIVE_QUEUE_TICKETS.remove(key);
-                        break;
-                    }
-                }
-
-                // add the new assignment
-                ObjectNode ticketAssignment = JsonNodeFactory.instance.objectNode();
-                ticketAssignment.put("status", status);
-                ticketAssignment.put("ticketNumber", ticketNumber);
-
-                ACTIVE_QUEUE_TICKETS.put(servicePointName, ticketAssignment);
-
-            }
-        }
-    }
-
-    public static ObjectNode getActiveTicketAssignments() {
-        return ACTIVE_QUEUE_TICKETS;
-    }
+	
+	/**
+	 * The object has: service point/room name as key for ease of search and update and object with
+	 * status and ticket number
+	 */
+	private static ObjectNode ACTIVE_QUEUE_TICKETS = JsonNodeFactory.instance.objectNode();
+	
+	/**
+	 * We want to control access to the ACTIVE_QUEUE_TICKETS so that requests are queued
+	 * 
+	 * @param servicePointName
+	 * @param ticketNumber
+	 * @param status
+	 */
+	synchronized public static void updateTicketAssignment(String servicePointName, String ticketNumber, String status) {
+		if (StringUtils.isNotBlank(servicePointName) && StringUtils.isNotBlank(ticketNumber)
+		        && StringUtils.isNotBlank(status)) {
+			if (ACTIVE_QUEUE_TICKETS.has(servicePointName)) { // check if there is an existing ticket assignment
+				//update the object with new information
+				ObjectNode tNode = (ObjectNode) ACTIVE_QUEUE_TICKETS.get(servicePointName);
+				tNode.put("status", status);
+				tNode.put("ticketNumber", ticketNumber);
+				ACTIVE_QUEUE_TICKETS.put(servicePointName, tNode);
+			} else {
+				// remove the ticket number from any assignment
+				
+				Iterator<String> keys = ACTIVE_QUEUE_TICKETS.getFieldNames();
+				
+				while (keys.hasNext()) { // check if the ticket number has active assignment to any of the service rooms
+					String key = keys.next();
+					ObjectNode obj = (ObjectNode) ACTIVE_QUEUE_TICKETS.get(key);
+					if (obj.has(ticketNumber)) {
+						// remove the object
+						ACTIVE_QUEUE_TICKETS.remove(key);
+						break;
+					}
+				}
+				
+				// add the new assignment
+				ObjectNode ticketAssignment = JsonNodeFactory.instance.objectNode();
+				ticketAssignment.put("status", status);
+				ticketAssignment.put("ticketNumber", ticketNumber);
+				
+				ACTIVE_QUEUE_TICKETS.put(servicePointName, ticketAssignment);
+				System.out.println("Object: " + ACTIVE_QUEUE_TICKETS.toString());
+				
+			}
+		}
+	}
+	
+	public static ObjectNode getActiveTicketAssignments() {
+		return ACTIVE_QUEUE_TICKETS;
+	}
+	
+	/**
+	 * Extracts the request body and return it as string
+	 *
+	 * @param reader
+	 * @return
+	 */
+	public static String fetchRequestBody(BufferedReader reader) {
+		String requestBodyJsonStr = "";
+		try {
+			
+			BufferedReader br = new BufferedReader(reader);
+			String output = "";
+			while ((output = reader.readLine()) != null) {
+				requestBodyJsonStr += output;
+			}
+		}
+		catch (IOException e) {
+			
+			System.out.println("IOException: " + e.getMessage());
+			
+		}
+		return requestBodyJsonStr;
+	}
 }
---
+++ omod/src/main/java/org/openmrs/module/queue/web/PatientQueueRestController.java
@@ -0,0 +1,82 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.queue.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.queue.api.digitalSignage.QueueTicketAssignments;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * The main controller that exposes additional end points for order entry
+ */
+@Controller
+@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/queueutil")
+public class PatientQueueRestController extends BaseRestController {
+	
+	protected final Log log = LogFactory.getLog(getClass());
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/assignticket")
+	@ResponseBody
+	public Object assignTicketToServicePoint(HttpServletRequest request) {
+		String requestBody = null;
+		try {
+			
+			requestBody = QueueTicketAssignments.fetchRequestBody(request.getReader());
+			
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			String msg = e.getMessage();
+			
+			return "Error extracting request body" + msg;
+		}
+		
+		if (requestBody != null) {
+			
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode actualObj = null;
+			try {
+				actualObj = mapper.readTree(requestBody);
+			}
+			catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			
+			String servicePointName = actualObj.get("servicePointName").textValue();
+			String ticketNumber = actualObj.get("ticketNumber").textValue();
+			String status = actualObj.get("status").textValue();
+			
+			QueueTicketAssignments.updateTicketAssignment(servicePointName, ticketNumber, status);
+			return "Successfully updated the ticket";
+		}
+		return "The request could not be interpreted!";
+	}
+	
+	/**
+	 * @see BaseRestController#getNamespace()
+	 */
+	
+	@Override
+	public String getNamespace() {
+		return "v1/queueutil";
+	}
+}
