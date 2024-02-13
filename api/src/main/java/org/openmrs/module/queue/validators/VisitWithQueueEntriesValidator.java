ca5a2af00af7b8601ecbfe05886f1968a2945982
Michael Seaton <mseaton@pih.org>
2024-02-13 18:03:14 -0500

    O3-2862 - Queue Module - validate that queue entry falls within visit when saving visit

---
+++ api/src/main/java/org/openmrs/module/queue/validators/VisitWithQueueEntriesValidator.java
@@ -0,0 +1,77 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.queue.validators;
+
+import java.util.Date;
+import java.util.List;
+
+import org.openmrs.Visit;
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.queue.api.QueueEntryService;
+import org.openmrs.module.queue.api.search.QueueEntrySearchCriteria;
+import org.openmrs.module.queue.model.QueueEntry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Handler(supports = { Visit.class }, order = 60)
+public class VisitWithQueueEntriesValidator implements Validator {
+	
+	private final QueueEntryService queueEntryService;
+	
+	@Autowired
+	public VisitWithQueueEntriesValidator(@Qualifier("queue.QueueEntryService") QueueEntryService queueEntryService) {
+		this.queueEntryService = queueEntryService;
+	}
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Visit.class.isAssignableFrom(clazz);
+	}
+	
+	@Override
+	public void validate(Object target, Errors errors) {
+		if (!(target instanceof Visit)) {
+			throw new IllegalArgumentException("the parameter target must be of type " + Visit.class);
+		}
+		Visit visit = (Visit) target;
+		
+		// This implementation is copied to match the core validator approach to nested encounters
+		if (visit.getId() != null) {
+			Date startDateTime = visit.getStartDatetime();
+			Date stopDateTime = visit.getStopDatetime();
+			
+			QueueEntrySearchCriteria criteria = new QueueEntrySearchCriteria();
+			criteria.setIsEnded(null);
+			criteria.setVisit(visit);
+			List<QueueEntry> queueEntries = queueEntryService.getQueueEntries(criteria);
+			for (QueueEntry queueEntry : queueEntries) {
+				if (queueEntry.getStartedAt().before(startDateTime)) {
+					errors.rejectValue("startDatetime", "Visit.queueEntriesCannotStartBeforeStartDate",
+					    "This visit has queue entries whose dates cannot be before the start date");
+					break;
+				}
+				if (stopDateTime != null) {
+					if (queueEntry.getStartedAt().after(stopDateTime)) {
+						errors.rejectValue("stopDatetime", "Visit.queueEntriesCannotStartAfterStopDate",
+						    "This visit has queue entries which start after the stop date");
+						break;
+					}
+					if (queueEntry.getEndedAt() != null && queueEntry.getEndedAt().after(stopDateTime)) {
+						errors.rejectValue("stopDatetime", "Visit.queueEntriesCannotEndAfterStopDate",
+						    "This visit has queue entries which end after the stop date");
+						break;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ api/src/test/java/org/openmrs/module/queue/validators/VisitWithQueueEntriesValidatorTest.java
@@ -0,0 +1,133 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.queue.validators;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.time.DateUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.Visit;
+import org.openmrs.module.queue.SpringTestConfiguration;
+import org.openmrs.module.queue.api.QueueEntryService;
+import org.openmrs.module.queue.model.QueueEntry;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.validation.BindException;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+@ContextConfiguration(classes = SpringTestConfiguration.class, inheritLocations = false)
+public class VisitWithQueueEntriesValidatorTest extends BaseModuleContextSensitiveTest {
+	
+	private static final List<String> INITIAL_DATASET_XML = Arrays.asList(
+	    "org/openmrs/module/queue/api/dao/QueueDaoTest_locationInitialDataset.xml",
+	    "org/openmrs/module/queue/api/dao/QueueEntryDaoTest_conceptsInitialDataset.xml",
+	    "org/openmrs/module/queue/api/dao/QueueEntryDaoTest_patientInitialDataset.xml",
+	    "org/openmrs/module/queue/api/dao/VisitQueueEntryDaoTest_visitInitialDataset.xml",
+	    "org/openmrs/module/queue/api/dao/QueueDaoTest_initialDataset.xml",
+	    "org/openmrs/module/queue/api/dao/QueueEntryDaoTest_initialDataset.xml",
+	    "org/openmrs/module/queue/validators/QueueEntryValidatorTest_globalPropertyInitialDataset.xml");
+	
+	private Visit visit;
+	
+	private QueueEntry queueEntry;
+	
+	private Errors errors;
+	
+	@Autowired
+	@Qualifier("queue.QueueEntryService")
+	private QueueEntryService queueEntryService;
+	
+	@Autowired
+	private VisitWithQueueEntriesValidator validator;
+	
+	@Before
+	public void setup() {
+		INITIAL_DATASET_XML.forEach(this::executeDataSet);
+		queueEntry = queueEntryService.getQueueEntryById(3).get();
+		visit = queueEntry.getVisit();
+		errors = new BindException(visit, visit.getClass().getName());
+	}
+	
+	@Test
+	public void validatorNotNull() {
+		assertNotNull(validator);
+	}
+	
+	@Test
+	public void shouldSupportVisit() {
+		assertTrue(validator.supports(Visit.class));
+	}
+	
+	@Test
+	public void shouldNotRejectQueueEntryByDefault() {
+		validator.validate(visit, errors);
+		assertFalse(errors.hasErrors());
+	}
+	
+	@Test
+	public void shouldNotRejectIfQueueEntryStartedAtEqualsVisitStartDate() {
+		visit.setStartDatetime(queueEntry.getStartedAt());
+		validator.validate(visit, errors);
+		assertFalse(errors.hasErrors());
+	}
+	
+	@Test
+	public void shouldNotRejectIfQueueEntryStartedAtEqualsVisitEndDate() {
+		visit.setStopDatetime(queueEntry.getStartedAt());
+		validator.validate(visit, errors);
+		assertFalse(errors.hasErrors());
+	}
+	
+	@Test
+	public void shouldNotRejectIfQueueEntryEndedAtAtEqualsVisitEndDate() {
+		queueEntry.setEndedAt(queueEntry.getStartedAt());
+		queueEntryService.saveQueueEntry(queueEntry);
+		visit.setStopDatetime(queueEntry.getEndedAt());
+		validator.validate(visit, errors);
+		assertFalse(errors.hasErrors());
+	}
+	
+	@Test
+	public void shouldRejectIfQueueEntryStartedBeforeVisitStartDate() {
+		visit.setStartDatetime(DateUtils.addMilliseconds(queueEntry.getStartedAt(), 1));
+		validator.validate(visit, errors);
+		FieldError queueEntryStatusFieldError = errors.getFieldError("startDatetime");
+		assertNotNull(queueEntryStatusFieldError);
+		assertThat(queueEntryStatusFieldError.getCode(), is("Visit.queueEntriesCannotStartBeforeStartDate"));
+	}
+	
+	@Test
+	public void shouldRejectIfQueueEntryStartedAfterVisitEndDate() {
+		visit.setStopDatetime(DateUtils.addMilliseconds(queueEntry.getStartedAt(), -1));
+		validator.validate(visit, errors);
+		FieldError queueEntryStatusFieldError = errors.getFieldError("stopDatetime");
+		assertNotNull(queueEntryStatusFieldError);
+		assertThat(queueEntryStatusFieldError.getCode(), is("Visit.queueEntriesCannotStartAfterStopDate"));
+	}
+	
+	@Test
+	public void shouldRejectIfQueueEntryEndedAfterVisitEndDate() {
+		queueEntry.setEndedAt(DateUtils.addHours(queueEntry.getStartedAt(), 1));
+		queueEntryService.saveQueueEntry(queueEntry);
+		visit.setStopDatetime(DateUtils.addMilliseconds(queueEntry.getEndedAt(), -1));
+		validator.validate(visit, errors);
+		FieldError queueEntryStatusFieldError = errors.getFieldError("stopDatetime");
+		assertNotNull(queueEntryStatusFieldError);
+		assertThat(queueEntryStatusFieldError.getCode(), is("Visit.queueEntriesCannotEndAfterStopDate"));
+	}
+}
