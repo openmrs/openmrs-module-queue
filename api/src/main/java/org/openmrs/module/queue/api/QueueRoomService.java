32ddeb7e12691a890e1eaefa38509a2a88595b0a
Michael Seaton <mseaton@pih.org>
2024-04-17 09:01:52 -0400

    Revert "O3-3002: Queue Module - REST endpoints can be accessed without authen…"
    This reverts commit 43117c92186a8451da60fe6891140f7d9fe97dec.

--- api/src/main/java/org/openmrs/module/queue/api/QueueRoomService.java
+++ api/src/main/java/org/openmrs/module/queue/api/QueueRoomService.java
@@ -14,73 +14,23 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
 
-import org.openmrs.annotation.Authorized;
 import org.openmrs.api.APIException;
 import org.openmrs.module.queue.api.search.QueueRoomSearchCriteria;
 import org.openmrs.module.queue.model.QueueRoom;
-import org.openmrs.module.queue.utils.PrivilegeConstants;
 
 public interface QueueRoomService {
 	
-	/**
-	 * Gets a queue room given a uuid.
-	 *
-	 * @param uuid uuid of the queue room to be returned.
-	 * @return {@link org.openmrs.module.queue.model.QueueRoom}
-	 */
-	@Authorized({ PrivilegeConstants.GET_QUEUE_ROOMS })
 	Optional<QueueRoom> getQueueRoomByUuid(@NotNull String uuid);
 	
-	/**
-	 * Gets a queue room by id.
-	 *
-	 * @param id the id of the queue room to retrieve.
-	 * @return {@link org.openmrs.module.queue.model.QueueRoom}
-	 */
-	@Authorized({ PrivilegeConstants.GET_QUEUE_ROOMS })
 	Optional<QueueRoom> getQueueRoomById(@NotNull int id);
 	
-	/**
-	 * Gets a List of all Queue Rooms.
-	 *
-	 * @return {@link List} of all queue rooms
-	 */
-	@Authorized({ PrivilegeConstants.GET_QUEUE_ROOMS })
 	List<QueueRoom> getAllQueueRooms();
 	
-	/**
-	 * Saves a queue room
-	 *
-	 * @param queueRoom the queue room to be saved
-	 * @return saved {@link org.openmrs.module.queue.model.QueueRoom}
-	 */
-	@Authorized({ PrivilegeConstants.MANAGE_QUEUE_ROOMS })
 	QueueRoom saveQueueRoom(@NotNull QueueRoom queueRoom);
 	
-	/**
-	 * Gets a List of all Queue Rooms that match the given QueueRoomSearchCriteria.
-	 *
-	 * @return {@link List} of queue rooms that match the given
-	 *         {@link org.openmrs.module.queue.api.search.QueueRoomSearchCriteria}
-	 */
-	@Authorized({ PrivilegeConstants.GET_QUEUE_ROOMS })
 	List<QueueRoom> getQueueRooms(QueueRoomSearchCriteria searchCriteria);
 	
-	/**
-	 * Retires a queue room.
-	 *
-	 * @param queueRoom the queue room to retire
-	 * @param retireReason the reason for retiring the queue room
-	 */
-	@Authorized({ PrivilegeConstants.MANAGE_QUEUE_ROOMS })
-	void retireQueueRoom(@NotNull QueueRoom queueRoom, String retireReason);
+	void retireQueueRoom(@NotNull QueueRoom queueRoom, String voidReason);
 	
-	/**
-	 * Completely remove a queue room from the database
-	 *
-	 * @param queueRoom queue room to be deleted
-	 * @throws org.openmrs.api.APIException
-	 */
-	@Authorized({ PrivilegeConstants.PURGE_QUEUE_ROOMS })
 	void purgeQueueRoom(@NotNull QueueRoom queueRoom) throws APIException;
 }
